30f750354fb4f11fd1122f44e3a79097ce971380
rahulroy9202 <rahulroy9202@gmail.com>
2015-04-07 17:32:00 +0530

    heroku port config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	ipaddress : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-	port : process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	port : process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
 	dburl : "mongodb://cloud9:cloud9@kahana.mongohq.com:10099/doba",
 	domains : [
 		'http://urls.rahulroy9202.in',
